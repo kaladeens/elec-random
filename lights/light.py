1dbc5c4c055ab3dde6f8ef471baca4505274ad1f
Sam <sabd0044@student.monash.edu>
2023-10-29 01:07:37 +1100

    require,emts + lights stuff done a bit

--- lights/light.py
+++ lights/light.py
@@ -6,7 +6,14 @@ led1 = board.get_pin('p:11:p')
 led2 = board.get_pin('d:10:p')
 led3 = board.get_pin('d:9:p')
 
+button = board.get_pin('d:2:i')
+
+def pressButton():
+    state = button.read()
+    print(state)
+
 while True:
     led1.write(.5)
     led2.write(.5)
     led3.write(.8)
+    pressButton()
