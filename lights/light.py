77861937d181fe347117c16aead0f3a009f3524b
Sam <sabd0044@student.monash.edu>
2023-10-29 00:43:07 +1100

    figured out how to write with pwm

--- lights/light.py
+++ lights/light.py
@@ -2,11 +2,11 @@ import pyfirmata
 
 board = pyfirmata.Arduino('COM5')
 
-led1 = board.get_pin('d:11:o')
-led2 = board.get_pin('d:10:o')
-led3 = board.get_pin('d:9:o')
+led1 = board.get_pin('p:11:p')
+led2 = board.get_pin('d:10:p')
+led3 = board.get_pin('d:9:p')
 
 while True:
-    led1.write(1)
-    led2.write(0)
-    led3.write(0)
+    led1.write(.5)
+    led2.write(.5)
+    led3.write(.8)
