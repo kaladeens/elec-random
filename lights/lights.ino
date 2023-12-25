1ef2b3621cb35d384ee4edfac0e506b1f17aa123
Sam <sabd0044@student.monash.edu>
2023-12-26 00:53:59 +1100

    did a lights thing so that when you move joystick around it will work like a colour wheel isnt super accurate but good enough

---
+++ lights/lights.ino
@@ -0,0 +1,81 @@
+
+const int greenPin = 10;
+const int bluePin = 11;
+const int redPin = 9;
+const int xPin = A0;
+const int yPin = A1;
+const int swPin = 13;
+
+void setup(){
+    Serial.begin(115200);
+    pinMode(bluePin, OUTPUT);
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(xPin, INPUT);
+    pinMode(yPin, INPUT);
+    pinMode(swPin, INPUT_PULLUP);
+}
+
+
+void loop(){
+    int xValue = analogRead(xPin);
+    int yValue = analogRead(yPin);
+    int swValue = digitalRead(swPin);
+    changeColour(xValue, yValue, swValue);
+
+}
+
+void changeColour(int xValue,int yValue, int swValue){
+    if(swValue == 0){
+        analogWrite(redPin, 0);
+        analogWrite(greenPin, 0);
+        analogWrite(bluePin, 0);
+    }
+    else{
+        int topC = map(yValue, 1023, 512, 255, 0);
+        int rightC = map(xValue, 512, 1023, 0, 255);
+        int leftC = map(xValue, 512, 0, 0, 255);
+        int botC = map(yValue, 0, 512, 255, 0);
+        Serial.print("topC: ");
+        Serial.println(topC);
+        Serial.print("rightC: ");
+        Serial.println(rightC);
+        Serial.print("leftC: ");
+        Serial.println(leftC);
+        Serial.print("botC: ");
+        Serial.println(botC);
+        if(topC < 0){
+            topC = 0;
+        }
+        if(rightC < 0){
+            rightC = 0;
+        }
+        if(leftC < 0){
+            leftC = 0;
+        }
+        if (botC < 6){
+            botC = 0;
+        }
+        if (botC > 255){
+            botC = 255;
+        }
+        if(topC > 255){
+            topC = 255;
+        }
+        if(rightC > 255){
+            rightC = 255;
+        }
+        if(leftC > 255){
+            leftC = 255;
+        }
+        if (topC == 0 && rightC == 0 && leftC == 0 && botC == 0){
+            topC = 255;
+            rightC = 255;
+            leftC = 255;
+        }
+        analogWrite(redPin, topC);
+        analogWrite(greenPin, rightC);
+        analogWrite(bluePin, leftC);
+       
+    }
+}
